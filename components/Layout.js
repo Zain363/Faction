8a276e39fa5ad95ecf72523c0f1fcfc850d0818d
Zain363 <81357783+Zain363@users.noreply.github.com>
2022-04-27 10:39:56 -0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ import Footer from '../components/Footer'
 function Layout() {
   return (
     
-    <div className="dark:bg-slate-900 text-center sm:text-left">
+    <div className="dark:bg-slate-900 text-center sm:text-left inline-block">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <Nav />
         <Main />
