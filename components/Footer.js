5c5e87ccaf5a9d08c4359c5ec9d0bce24b9a0864
Zain363 <81357783+Zain363@users.noreply.github.com>
2022-05-01 10:28:41 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
  
 <div>
   
-<div className=" py-12  flex-col items-center sm:flex-row sm:justify-between text-sm text-gray-400">© Copyright 2021. All Rights Reserved.
+<div className=" py-12  container  flex-col items-center sm:flex-row sm:justify-between text-sm text-gray-400">© Copyright 2021. All Rights Reserved.
 </div>
 
 
