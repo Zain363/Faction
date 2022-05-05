439968e45fbefc03cfa9e8dcd3e453202cd29d0b
Zain363 <81357783+Zain363@users.noreply.github.com>
2022-05-05 16:45:43 -0700

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -12,7 +12,7 @@ function Main() {
            <div className="flex justify-center font-extrabold title-font sm:text-4xl text-3xl mb-4 font-bold dark:text-white">Create your own web3 </div>
            <span className=" -mt-2 justify-center font-extrabold title-font sm:text-4xl text-3xl mb-4 font-bold bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">Community</span>
             <div className=" py-12  flex justify-center items-center mb-8 leading-relaxed italic dark:text-slate-400 text-center sm:text-left">
-            Send private messages, Private groups chats, Create DAO and join or create public servers to connect with your community.
+            Send private messages, Private groups chats, and join or create public servers to connect with your community.
             </div>
              
 
